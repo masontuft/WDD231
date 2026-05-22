1175fbabc23c3e4e8796e01c6dd4b5d362b5a199
JMRCycling <mtuft1@byui.edu>
2026-05-21 23:07:08 -0600

    ponder

---
+++ src/api.js
@@ -0,0 +1,24 @@
+// api.js
+const baseUrl = "https://developer.nps.gov/api/v1/";
+
+async function getJson(endpoint) {
+  // replace this with your actual key
+  const apiKey = "hezrHC7hDpneKVsU7ry9S9ZTxPgECdwTKFFw0h8Z";
+  // construct the url: baseUrl + endpoint + parameters
+  const url = baseUrl + endpoint;
+  // set the options. The important one here is the X-Api-Key
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Api-Key": apiKey
+      }
+  }
+  // make the request
+  const response = await fetch(url, options)
+  const data = await response.json()
+  console.log(data)
+  return data;
+}
+
+getJson('alerts?parkCode=acad,dena');
